44ca673f1b0f6fa7c7c7f58360bce83919b8e6fb
Ralph Joachim <raljoach@gmail.com>
2020-12-26 06:55:26 -0800

    Email project created

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Email.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {        
+        private readonly ILogger<EmailController> _logger;
+
+        public EmailController(ILogger<EmailController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Status()
+        {
+            return "Service is available";
+        }
+    }
+}
---
+++ EmailData.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Email
+{
+    public class EmailData
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
